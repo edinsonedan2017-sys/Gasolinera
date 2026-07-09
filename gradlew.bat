586fe4ff5b91d40bd0b93c8e9704193d6d97db69
edinsonedan2017-sys <edinsonedan2017@gmail.com>
2026-07-08 20:50:12 -0500

    Add files via upload

---
+++ gradlew.bat
@@ -0,0 +1,11 @@
+@rem Script de Gradle para Windows
+@if "%DEBUG%"=="" @echo off
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
